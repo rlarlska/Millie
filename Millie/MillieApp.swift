f87f0598f3ceeb49ad0df2fe70fd889417b5a54c
rlarlska <dkffla89@naver.com>
2024-05-07 19:18:54 +0900

    Initial Commit

---
+++ Millie/MillieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillieApp.swift
+//  Millie
+//
+//  Created by 김기남 on 2024/05/07.
+//
+
+import SwiftUI
+
+@main
+struct MillieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
